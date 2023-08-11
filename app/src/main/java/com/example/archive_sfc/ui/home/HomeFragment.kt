209c952cfba8c942f81e53791a369a5ac5565eac
Elie Oko <elieoko100@gmail.com>
2023-08-11 11:00:28 +0100

    feat:getAll invoice-picture in UI

--- app/src/main/java/com/example/archive_sfc/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/archive_sfc/ui/home/HomeFragment.kt
@@ -1,21 +1,39 @@
 package com.example.archive_sfc.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.archive_sfc.UserApplication
+import com.example.archive_sfc.adaptater.AdaptaterImageStore
+import com.example.archive_sfc.constante.Data
 import com.example.archive_sfc.databinding.FragmentHomeBinding
+import com.example.archive_sfc.models.room.Picture
+import com.example.archive_sfc.room.invoice.viewmodel.InvoiceViewModel
+import com.example.archive_sfc.room.invoice.viewmodel.InvoiceViewModelFactory
+import com.example.archive_sfc.room.picture.viewmodel.PictureViewModel
+import com.example.archive_sfc.room.picture.viewmodel.PictureViewModelFactory
 
 class HomeFragment : Fragment() {
+    private var binding: FragmentHomeBinding? = null
+    private var adapterImageStore : AdaptaterImageStore? =null
+    private lateinit var  recyclerView: RecyclerView
 
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    val pictureViewModel: PictureViewModel by viewModels {
+        PictureViewModelFactory((activity?.application as UserApplication).repositoryPicture)
+    }
+    val invoiceViewModel: InvoiceViewModel by viewModels {
+        InvoiceViewModelFactory((activity?.application  as UserApplication).repositoryInvoice)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,20 +41,55 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this)[HomeViewModel::class.java]
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+//        val textView: TextView = binding!!.textHome
+//        homeViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+        initRecy()
+        return binding!!.root
     }
+/*
+ */
+override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+    super.onViewCreated(view, savedInstanceState)
 }
+    private fun initRecy() {
+// 123456789//MA
+        Toast.makeText(requireContext(),"Elie Oko",Toast.LENGTH_LONG).show()
+        adapterImageStore = AdaptaterImageStore()
+        recyclerView = binding!!.recyclerViewImage
+        recyclerView.layoutManager = GridLayoutManager(requireContext(),2)
+        recyclerView.adapter = adapterImageStore
+        val listInvoicePicture: ArrayList<List<Picture>> = ArrayList<List<Picture>>()
+        invoiceViewModel.allInvoice.observe(this){
+
+            var iteration = 0
+            it.forEach {i->
+                pictureViewModel.getAllImageByInvoice(i.InvoiceId).observe(this){listPicture ->
+                    Log.d("Iteration -->","$it")
+                    Log.d("Voir plus grand =>","$listPicture")
+//                val item:ArrayList<ArrayList<List<Picture>>> = ArrayList()
+                    //item.add(listPicture)
+                    if(!listInvoicePicture.contains(listPicture)){
+                        listInvoicePicture.addAll(arrayListOf(listPicture))
+                        Log.d("Voir plus haut =>","$listInvoicePicture")
+                        Log.e("Iteration awa ->","Taille ${listInvoicePicture.size}")
+                    }
+                    iteration++
+                    if( it.size  == iteration){
+                        Data.allInvoicePicture.addAll(listInvoicePicture)
+                        adapterImageStore?.addImageInContenaire(listInvoicePicture)
+                        Log.e("Akoti na ->","Taille Oyo ${listInvoicePicture.size}")
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+

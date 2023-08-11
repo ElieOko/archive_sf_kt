8bca529b294a0f3f314837f5e5413dc90f8af65b
Elie Oko <elieoko100@gmail.com>
2023-08-11 10:59:01 +0100

    buidl:review constante/ folder

--- app/src/main/java/com/example/archive_sfc/constante/Data.kt
+++ app/src/main/java/com/example/archive_sfc/constante/Data.kt
@@ -1,7 +1,9 @@
 package com.example.archive_sfc.constante
 
+import com.example.archive_sfc.models.room.Picture
 import com.example.archive_sfc.models.room.User
 
 object Data {
     var user : User? = null
+    val allInvoicePicture:ArrayList<List<Picture>> = ArrayList()
 }
--- app/src/main/java/com/example/archive_sfc/constante/ImageParcours.kt
+++ app/src/main/java/com/example/archive_sfc/constante/ImageParcours.kt
@@ -1,7 +1,9 @@
 package com.example.archive_sfc.constante
 
 import com.example.archive_sfc.models.FileState
+import java.io.File
 
 object ImageParcours {
   var stdList :ArrayList<FileState> = ArrayList()
+  var storeFileTemp : File? = null
 }

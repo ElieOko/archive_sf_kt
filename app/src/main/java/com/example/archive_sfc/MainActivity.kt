bf9c8153dd2ad701ce5423daca3934498b460d7f
Elie Oko <elieoko100@gmail.com>
2023-08-11 11:02:35 +0100

    feat:get all data invoice-picture

--- app/src/main/java/com/example/archive_sfc/MainActivity.kt
+++ app/src/main/java/com/example/archive_sfc/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.ExperimentalGetImage
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -23,15 +24,18 @@ import com.example.archive_sfc.constante.Route
 import com.example.archive_sfc.constante.UserData
 import com.example.archive_sfc.databinding.ActivityMainBinding
 import com.example.archive_sfc.databinding.NavHeaderMainBinding
-import com.example.archive_sfc.models.ApiResponseDirectory
-import com.example.archive_sfc.models.ApiResponseInvoiceKey
 import com.example.archive_sfc.models.room.Directory
 import com.example.archive_sfc.models.room.InvoiceKey
+import com.example.archive_sfc.models.room.Picture
 import com.example.archive_sfc.models.room.User
 import com.example.archive_sfc.room.directory.viewModel.DirectoryViewModel
 import com.example.archive_sfc.room.directory.viewModel.DirectoryViewModelFactory
+import com.example.archive_sfc.room.invoice.viewmodel.InvoiceViewModel
+import com.example.archive_sfc.room.invoice.viewmodel.InvoiceViewModelFactory
 import com.example.archive_sfc.room.invoicekey.viewmodel.InvoiceKeyViewModel
 import com.example.archive_sfc.room.invoicekey.viewmodel.InvoiceKeyViewModelFactory
+import com.example.archive_sfc.room.picture.viewmodel.PictureViewModel
+import com.example.archive_sfc.room.picture.viewmodel.PictureViewModelFactory
 import com.example.archive_sfc.room.user.viewmodel.UserViewModel
 import com.example.archive_sfc.room.user.viewmodel.UserViewModelFactory
 import com.example.archive_sfc.utils.url_fusio
@@ -39,16 +43,16 @@ import com.example.archive_sfc.volley.Singleton
 import com.google.android.material.navigation.NavigationView
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import dev.shreyaspatil.MaterialDialog.MaterialDialog
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
+
 
 @ExperimentalGetImage
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-    //private var adapter : AdaptaterMain? =null
-    //var profil :ArrayList<Profil> = ArrayList()
     private val userViewModel: UserViewModel by viewModels {
         UserViewModelFactory((application as UserApplication).repository)
     }
@@ -58,6 +62,13 @@ class MainActivity : AppCompatActivity() {
     private val invoiceKeyViewModel: InvoiceKeyViewModel by viewModels {
         InvoiceKeyViewModelFactory((application as UserApplication).repositoryInvoiceKey)
     }
+    private val pictureViewModel: PictureViewModel by viewModels {
+        PictureViewModelFactory((application as UserApplication).repositoryPicture)
+    }
+    private val invoiceViewModel: InvoiceViewModel by viewModels {
+        InvoiceViewModelFactory((application  as UserApplication).repositoryInvoice)
+    }
+    private val listInvoicePicture: ArrayList<List<Picture>> = ArrayList<List<Picture>>()
     @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -81,13 +92,11 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-
         val navigationView  : NavigationView = binding.navView
         val headerView = navigationView.getHeaderView(0)
 
         Toast.makeText(this,getVersion(),Toast.LENGTH_LONG).show()
         binding.appBarMain.fab.setOnClickListener {
-
             val intent = Intent(this@MainActivity, CameraActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
             startActivity(intent)
@@ -99,18 +108,47 @@ class MainActivity : AppCompatActivity() {
         val navViewHeaderBinding : NavHeaderMainBinding = NavHeaderMainBinding.bind(headerView)
         navViewHeaderBinding.name.text = UserData.name
         navViewHeaderBinding.status.text = UserData.status
+
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
             ), drawerLayout
         )
+        getAllInvoicePicture()
+        binding.appBarMain.toolbar.setOnMenuItemClickListener {
+            when(it.itemId){
+                R.id.action_save_all -> {
+                    sendAllInvoice()
+                    true
+                }
+               else ->{
+                   true
+               }
+            }
+        }
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-
-       // updateData(store)
+        val mDialog =  MaterialDialog.Builder(this)
+        mDialog.setAnimation(R.raw.animation_success)
+            .setTitle("Congratulation")
+            .setPositiveButton("Okey"){dialog,_->
+                dialog.dismiss()
+            }
+        mDialog.build().show()
     }
 
+    private fun sendAllInvoice() {
+       val msg =  postInvoicePictusre()
+        val mDialog =  MaterialDialog.Builder(this)
+        mDialog.setAnimation(R.raw.animation_refresh)
+            .setTitle("Collection save")
+            .setMessage(msg)
+            .setPositiveButton("Okey"){dialog,_->
+                dialog.dismiss()
+            }
+        mDialog.build().show()
+    }
 
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -132,7 +170,6 @@ class MainActivity : AppCompatActivity() {
                 Data.user = test
                 Log.d("Storage===>>>",test?.email.toString())
             }
-
         }
     }
 
@@ -184,7 +221,33 @@ class MainActivity : AppCompatActivity() {
             )
             Singleton.getInstance(applicationContext).addToRequestQueue(stringRequest)
         }
-
     }
-
+    private fun getAllInvoicePicture(){
+        invoiceViewModel.allInvoice.observe(this){
+            var iteration = 0
+            it.forEach {i->
+                pictureViewModel.getAllImageByInvoice(i.InvoiceId).observe(this){listPicture ->
+                    Log.d("Iteration -->","$it")
+                    Log.d("Voir plus grand =>","$listPicture")
+                    if(!listInvoicePicture.contains(listPicture)){
+                        listInvoicePicture.addAll(arrayListOf(listPicture))
+                        Log.d("Voir plus haut =>","$listInvoicePicture")
+                        Log.e("Iteration awa ->","Taille ${listInvoicePicture.size}")
+                    }
+                    iteration++
+                    if( it.size  == iteration){
+                        Data.allInvoicePicture.addAll(listInvoicePicture)
+                        Log.e("Akoti na ->","Taille Oyo ${listInvoicePicture.size}")
+                    }
+                }
+            }
+        }
+    }
+    private fun postInvoicePictusre():String{
+        var msg = "Nothing data to collection invoice"
+        if(listInvoicePicture.isNotEmpty()){
+            msg = "Data in collection invoice picture sum ${listInvoicePicture.size} "
+        }
+        return msg
+    }
 }

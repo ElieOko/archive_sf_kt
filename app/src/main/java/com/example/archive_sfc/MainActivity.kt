61bf108baade27db9b8fd67391b7ccbe8e9aee19
Elie Oko <elieoko100@gmail.com>
2023-08-14 03:00:03 +0100

    build:adapation call fragment

--- app/src/main/java/com/example/archive_sfc/MainActivity.kt
+++ app/src/main/java/com/example/archive_sfc/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.archive_sfc
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.ContextWrapper
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -11,13 +14,12 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.ExperimentalGetImage
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.example.archive_sfc.constante.Data
 import com.example.archive_sfc.constante.Route
@@ -38,6 +40,7 @@ import com.example.archive_sfc.room.picture.viewmodel.PictureViewModel
 import com.example.archive_sfc.room.picture.viewmodel.PictureViewModelFactory
 import com.example.archive_sfc.room.user.viewmodel.UserViewModel
 import com.example.archive_sfc.room.user.viewmodel.UserViewModelFactory
+import com.example.archive_sfc.ui.home.HomeFragment
 import com.example.archive_sfc.utils.url_fusio
 import com.example.archive_sfc.volley.Singleton
 import com.google.android.material.navigation.NavigationView
@@ -46,6 +49,10 @@ import com.google.gson.reflect.TypeToken
 import dev.shreyaspatil.MaterialDialog.MaterialDialog
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
+import org.json.JSONObject
+import java.io.File
+import java.io.FileOutputStream
+import java.util.*
 
 
 @ExperimentalGetImage
@@ -68,6 +75,7 @@ class MainActivity : AppCompatActivity() {
     private val invoiceViewModel: InvoiceViewModel by viewModels {
         InvoiceViewModelFactory((application  as UserApplication).repositoryInvoice)
     }
+    private val fragment = HomeFragment()
     private val listInvoicePicture: ArrayList<List<Picture>> = ArrayList<List<Picture>>()
     @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -75,6 +83,10 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+//        supportFragmentManager
+//            .beginTransaction()
+//            .add(R.id.frag, fragment , "MyFragment")
+//            .commit()
         userViewModel.user.observe(this){
             if(it != null){
                 Log.d("com",it.toString())
@@ -85,6 +97,7 @@ class MainActivity : AppCompatActivity() {
                 ).show()
             }
         }
+
         val user = User(0,UserData.name,UserData.password)
         setUser(user)
         setSupportActionBar(binding.appBarMain.toolbar)
@@ -114,7 +127,28 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
             ), drawerLayout
         )
-        getAllInvoicePicture()
+
+        binding.drawerLayout.setOnClickListener{
+
+        }
+        binding.navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.menu_logout ->{
+                    val intent = Intent(this@MainActivity, LoginActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+                    startActivity(intent)
+                    finish()
+                    true
+                }
+                R.id.menu_serve_url ->{
+                    launch()
+                    true
+                }
+                else ->{
+                    true
+                }
+            }
+        }
         binding.appBarMain.toolbar.setOnMenuItemClickListener {
             when(it.itemId){
                 R.id.action_save_all -> {
@@ -126,8 +160,8 @@ class MainActivity : AppCompatActivity() {
                }
             }
         }
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+       // setupActionBarWithNavController(navController, appBarConfiguration)
+        //navView.setupWithNavController(navController)
 
         val mDialog =  MaterialDialog.Builder(this)
         mDialog.setAnimation(R.raw.animation_success)
@@ -136,6 +170,10 @@ class MainActivity : AppCompatActivity() {
                 dialog.dismiss()
             }
         mDialog.build().show()
+    }
+
+    private fun launch() {
+
     }
 
     private fun sendAllInvoice() {
@@ -148,6 +186,81 @@ class MainActivity : AppCompatActivity() {
                 dialog.dismiss()
             }
         mDialog.build().show()
+        val invoiceData = JSONObject()
+        invoiceData.put("data",listInvoicePicture)
+//            invoiceViewModel.allInvoice.observe(this){
+//                GlobalScope.launch(Dispatchers.Main){
+//                    it.forEach {i->
+//                        val data = JSONObject()
+//                        data.put("id",i.InvoiceId)
+//                        data.put("branchfid",i.BranchFId)
+//                        data.put("directoryfid",i.DirectoryFId)
+//                        data.put("userfid",i.UserFId)
+//                        data.put("code",i.invoiceCode)
+//                        data.put("invoicekeyfid",i.InvoicekeyFId)
+//                        data.put("androidversion",i.androidVersion)
+//                        data.put("clientname",i.clientName)
+//                        data.put("clientphone",i.clientPhone)
+//                        data.put("invoiceuniqueid",i.invoiceUniqueId)
+//                        data.put("invoicepath",i.invoicePath)
+//                        data.put("invoicedesc",i.invoiceDesc)
+//                        data.put("invoicedate",i.invoiceDate)
+//                        data.put("expireddate",i.expiredDate)
+//                        data.put("invoicebarcode",i.invoiceBarCode)
+//                        val jsonObjectRequest = JsonObjectRequest(
+//                            Request.Method.POST, url_fusio(Route.url,Route.postInvoice_endpoint), data,
+//                            { response ->
+//                                Log.e("Viva","coding -> $response")
+//                                val responseObject: User = Gson().fromJson(response.toString(),object : TypeToken<User>() {}.type)
+//                                Log.d("Login","Oups->> $responseObject")
+//
+//                                pictureViewModel.getAllImageByInvoice(i.InvoiceId).observe(this@MainActivity){listPicture ->
+//                                    listPicture.forEach { picture->
+//                                        val dataPicture = JSONObject()
+//                                        dataPicture.put("pictureid",picture.PictureId)
+//                                        dataPicture.put("invoiceid",picture.InvoiceFId)
+//                                        dataPicture.put("publicurl",picture.PublicUrl)
+//                                        dataPicture.put("picturename",picture.pictureName)
+//                                        dataPicture.put("picturepath",picture.picturePath)
+//                                        val wrapper = ContextWrapper(applicationContext)
+//                                        var file = wrapper.getDir("Images", Context.MODE_PRIVATE)
+//                                        file = File(file,"${UUID.randomUUID()}.jpg")
+//                                        val stream= FileOutputStream(file)
+//                                        picture.contentFile?.compress(Bitmap.CompressFormat.JPEG,25,stream)
+//                                        stream.flush()
+//                                        stream.close()
+//                                        dataPicture.put("image",file)
+//                                        val request = JsonObjectRequest(
+//                                            Request.Method.POST, url_fusio(Route.url,Route.postPicture_endpoint), dataPicture,
+//                                            { response ->
+//
+//                                            },
+//                                            { error ->
+//
+//                                            })
+//                                        Singleton.getInstance(applicationContext).addToRequestQueue(request)
+//                                    }
+//                                }
+//
+//
+//                                mDialog.build().dismiss()
+//                            },
+//                            { error ->
+//                                mDialog.build().dismiss()
+//                                if(error.toString() == "com.android.volley.ClientError"){
+//                                    //mDialogConnexion("Error","We couldn't find the server",R.raw.animation_srv)
+//                                }
+//                                Toast.makeText(applicationContext,"Error",Toast.LENGTH_LONG).show()
+//                            }
+//                        )
+//                        Singleton.getInstance(applicationContext).addToRequestQueue(jsonObjectRequest)
+//                    }
+//                }
+//
+//            }
+
+
+
     }
 
 
@@ -222,31 +335,33 @@ class MainActivity : AppCompatActivity() {
             Singleton.getInstance(applicationContext).addToRequestQueue(stringRequest)
         }
     }
-    private fun getAllInvoicePicture(){
-        invoiceViewModel.allInvoice.observe(this){
-            var iteration = 0
-            it.forEach {i->
-                pictureViewModel.getAllImageByInvoice(i.InvoiceId).observe(this){listPicture ->
-                    Log.d("Iteration -->","$it")
-                    Log.d("Voir plus grand =>","$listPicture")
-                    if(!listInvoicePicture.contains(listPicture)){
-                        listInvoicePicture.addAll(arrayListOf(listPicture))
-                        Log.d("Voir plus haut =>","$listInvoicePicture")
-                        Log.e("Iteration awa ->","Taille ${listInvoicePicture.size}")
-                    }
-                    iteration++
-                    if( it.size  == iteration){
-                        Data.allInvoicePicture.addAll(listInvoicePicture)
-                        Log.e("Akoti na ->","Taille Oyo ${listInvoicePicture.size}")
-                    }
-                }
-            }
-        }
-    }
+//    private fun getAllInvoicePicture(){
+//        invoiceViewModel.allInvoice.observe(this){
+//            var iteration = 0
+//            it.forEach {i->
+//                pictureViewModel.getAllImageByInvoice(i.InvoiceId).observe(this){listPicture ->
+//                    Log.d("Iteration -->","$it")
+//                    Log.d("Voir plus grand =>","$listPicture")
+//                    if(!listInvoicePicture.contains(listPicture)){
+//                        listInvoicePicture.addAll(arrayListOf(listPicture))
+//                        Log.d("Voir plus haut =>","$listInvoicePicture")
+//                        Log.e("Iteration awa ->","Taille ${listInvoicePicture.size}")
+//                    }
+//                    iteration++
+//                    if( it.size  == iteration){
+//                       // Data.allInvoicePicture.addAll(listInvoicePicture)
+//                        Log.e("Akoti na ->","Taille Oyo ${listInvoicePicture.size}")
+//
+//                    }
+//                }
+//            }
+//        }
+//    }
     private fun postInvoicePictusre():String{
+
         var msg = "Nothing data to collection invoice"
-        if(listInvoicePicture.isNotEmpty()){
-            msg = "Data in collection invoice picture sum ${listInvoicePicture.size} "
+        if(fragment.sizeAdd()!= 0){
+            msg = "Data in collection invoice picture sum ${fragment.sizeAdd()} "
         }
         return msg
     }

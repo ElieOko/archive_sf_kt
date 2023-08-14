44b814f09e6f14d5b003893b40ef9227736c37f8
Elie Oko <elieoko100@gmail.com>
2023-08-14 03:01:17 +0100

    build:review vm

--- app/src/main/java/com/example/archive_sfc/room/invoice/viewmodel/InvoiceViewModel.kt
+++ app/src/main/java/com/example/archive_sfc/room/invoice/viewmodel/InvoiceViewModel.kt
@@ -1,20 +1,28 @@
 package com.example.archive_sfc.room.invoice.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.example.archive_sfc.models.room.Invoice
+import com.example.archive_sfc.models.room.Picture
 import com.example.archive_sfc.room.invoice.repository.InvoiceRepository
 import kotlinx.coroutines.launch
 
 class InvoiceViewModel (private val repository: InvoiceRepository) : ViewModel()  {
 
-    val allInvoice: LiveData<List<Invoice>> = repository.allInvoice.asLiveData()
-    /**
-     * Launching a new coroutine to insert the data in a non-blocking way
-     */
+    val xs: LiveData<List<Invoice>> = repository.allInvoice.asLiveData()
+    
+    val allInvoice = repository.allInvoice.asLiveData()
+    var allInvoices : MutableLiveData<List<Invoice>> = MutableLiveData<List<Invoice>>()
+
     fun insert(invoice: Invoice) = viewModelScope.launch {
         repository.insert(invoice)
+    }
+
+    fun getAllInvoice(): MutableLiveData<List<Invoice>> {
+        viewModelScope.launch {
+            allInvoice.observeForever {
+                allInvoices.value = it
+            }
+        }
+        return allInvoices
     }
 }
